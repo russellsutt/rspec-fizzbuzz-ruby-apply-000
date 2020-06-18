704f3b1ba6813ae08d404683150d116f6fdecdc2
Russell Sutter <russell.sutter@gmail.com>
2020-06-18 22:22:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,3 @@ def fizzbuzz(i)
   if i / 3 == 0
     put "Fizz"
   end
-    
-    
-    
-  elsif int % 5 == 0
-    put "Buzz"
-  elsif int % 15 == 0
-    put "Fizzbuzz"
-  else int  == 0
-    put "NIL"
-  end
-end
